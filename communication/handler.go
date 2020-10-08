34969ef980c8d64a9b620f8b654c00301472153b
lucaschimweg <luca@schimweg.net>
2020-10-08 13:25:47 +0200

    KU-17 Use common component listener

---
+++ communication/handler.go
@@ -0,0 +1,29 @@
+package communication
+
+import (
+	"context"
+	commonCommunication "github.com/kulycloud/common/communication"
+	"github.com/kulycloud/common/logging"
+	protoRouteProcessor "github.com/kulycloud/protocol/route-processor"
+)
+
+
+var _ protoRouteProcessor.RouteProcessorServer = &RouteProcessorHandler{}
+
+var logger = logging.GetForComponent("handler")
+
+type RouteProcessorHandler struct {}
+
+func NewRouteProcessorHandler() *RouteProcessorHandler {
+	return &RouteProcessorHandler{}
+}
+
+func (handler *RouteProcessorHandler) Register(listener *commonCommunication.Listener) {
+	protoRouteProcessor.RegisterRouteProcessorServer(listener.Server, handler)
+}
+
+func (handler *RouteProcessorHandler) ProcessRoute(ctx context.Context, request *protoRouteProcessor.RouteProcessorRequest) (*protoRouteProcessor.RouteProcessorResponse, error) {
+	logger.Infow("processing route", "route", request.Data)
+	return &protoRouteProcessor.RouteProcessorResponse{Status: protoRouteProcessor.RouteProcessorStatus_OK}, nil
+}
+
